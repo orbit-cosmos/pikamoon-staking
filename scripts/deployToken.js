bb339897386c380780b1ebc4c2d4f3b959a055da
Asadullah khan <asadullah.khan@tigercrypto.uk>
2024-02-14 16:25:47 +0500

    edit deployment script

--- scripts/deployToken.js
+++ scripts/deployToken.js
@@ -3,7 +3,7 @@ const Token = require("../args/Token");
 async function main() {
   const [owner] = await ethers.getSigners();
   const ICOToken = await ethers.getContractFactory("ICOToken");
-  const token = await ICOToken.deploy(Token[0], Token[1], Token[2]);
+  const token = await ICOToken.deploy(Token[0], Token[1], Token[2],Token[3],Token[4]);
   await token.deployed();
 
   console.log("ICOToken", token.address);
